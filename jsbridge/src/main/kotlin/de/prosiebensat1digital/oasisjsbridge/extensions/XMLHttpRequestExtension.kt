2e72f32612db82df99de1c3ee0dc8182b6fef4f9
Mathias Schreck <mathias.schreck@joyn.de>
2021-10-01 12:38:35 +0200

    Support addEventListener method for XHR polyfill
    See for example https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/load_event

--- jsbridge/src/main/kotlin/de/prosiebensat1digital/oasisjsbridge/extensions/XMLHttpRequestExtension.kt
+++ jsbridge/src/main/kotlin/de/prosiebensat1digital/oasisjsbridge/extensions/XMLHttpRequestExtension.kt
@@ -176,6 +176,57 @@ internal class XMLHttpRequestExtension(
  */
 const val xhrJsCode: String = """
 (function() {
+
+function isListenerWithMatchingName(eventName) {
+    return function (listener) {
+        return listener.name === eventName;
+    };
+}
+
+function extractHandler(listener) {
+    return listener.handler;
+}
+
+function invokeHandler(eventPayload) {
+    return function (handler) {
+        handler(eventPayload);
+    };
+}
+
+function emit(eventListeners, eventName, eventPayload) {
+    var matchingListeners = eventListeners.filter(isListenerWithMatchingName(eventName)),
+        matchingHandlers = matchingListeners.map(extractHandler);
+
+    matchingHandlers.forEach(invokeHandler(eventPayload));
+}
+
+function noop() {}
+
+function createProgressEventPayload(options) {
+    return {
+        type: options.type,
+        bubbles: false,
+        cancelBubble: false,
+        cancelable: false,
+        composed: false,
+        defaultPrevented: false,
+        eventPhase: 0,
+        isTrusted: true,
+        lengthComputable: false,
+        loaded: -1,
+        path: [],
+        returnValue: true,
+        srcElement: options.xhr,
+        currentTarget: options.xhr,
+        target: options.xhr,
+        timeStamp: 0,
+        total: -1,
+        preventDefault: noop,
+        stopImmediatePropagation: noop,
+        stopPropagation: noop
+    };
+}
+
 var XMLHttpRequest = function() {
   this._send_native = XMLHttpRequestExtension_send_native;
 
@@ -183,6 +234,7 @@ var XMLHttpRequest = function() {
   this._url = null;
   this._requestHeaders = [];
   this._responseHeaders = [];
+  this._eventListeners = [];
 
   this.response = null;
   this.responseText = null;
@@ -240,6 +292,10 @@ XMLHttpRequest.prototype.send = function(data) {
   });
 };
 
+XMLHttpRequest.prototype.addEventListener = function(eventName, handler) {
+    this._eventListeners.push({ name: eventName, handler: handler });
+};
+
 XMLHttpRequest.prototype.abort = function() {
   this.readyState = XMLHttpRequest.UNSENT;
   // Note: this.onreadystatechange() is not supposed to be called according to the XHR specs
@@ -258,6 +314,7 @@ XMLHttpRequest.prototype._send_native_callback = function(responseInfo, response
       //console.log("Calling onabort()...");
       this.onabort();
     }
+    emit(this._eventListeners, 'abort', createProgressEventPayload({ type: 'abort', xhr: this }));
     return;
   }
 
@@ -323,6 +380,7 @@ XMLHttpRequest.prototype._send_native_callback = function(responseInfo, response
       //console.log("Calling ontimeout()...");
       this.ontimeout();
     }
+    emit(this._eventListeners, 'error', createProgressEventPayload({ type: 'error', xhr: this }));
   } else if (error) {
     // Error
     console.warn("Got XHR error:", error);
@@ -330,6 +388,7 @@ XMLHttpRequest.prototype._send_native_callback = function(responseInfo, response
       //console.log("Calling onerror()...");
       this.onerror();
     }
+    emit(this._eventListeners, 'error', createProgressEventPayload({ type: 'error', xhr: this }));
   } else {
     // Success
     //console.log("XHR success: response =", this.response);
@@ -337,12 +396,14 @@ XMLHttpRequest.prototype._send_native_callback = function(responseInfo, response
       //console.log("Calling onload()...");
       this.onload();
     }
+    emit(this._eventListeners, 'load', createProgressEventPayload({ type: 'load', xhr: this }));
   }
 
   if (typeof this.onloadend === "function") {
     //console.log("Calling onloadend()...");
     this.onloadend();
   }
+  emit(this._eventListeners, 'loadend', createProgressEventPayload({ type: 'loadend', xhr: this }));
 };
 
 XMLHttpRequest.prototype.setRequestHeader = function(header, value) {
