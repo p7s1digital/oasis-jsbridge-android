859159926d15d5ad7a75864ec6432a53db73aa08
Benoit <benoit.walter@cariad.technology>
2023-01-10 22:19:25 +0100

    Upgrade dependencies

--- jsbridge/src/main/kotlin/de/prosiebensat1digital/oasisjsbridge/Method.kt
+++ jsbridge/src/main/kotlin/de/prosiebensat1digital/oasisjsbridge/Method.kt
@@ -96,6 +96,7 @@ internal class Method {
                         inParameterTypes.add(genericArgumentType)
                     }
                 }
+                else -> Unit
             }
         }
 
